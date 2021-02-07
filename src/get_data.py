ac2a1ef78951d9eb8509bab2a1845d09a3a3ee8a
Artemis21 <artemisdev1@gmail.com>
2021-02-07 04:32:39 +0000

    Setup to download tweets (NOT TESTED)

--- src/get_data.py
+++ src/get_data.py
@@ -1,7 +1,12 @@
 """Tool to get the data from the Twitter API."""
+import json
+
 import requests
 
 from .config import BASE_PATH, TWITTER_TOKEN
+
+
+ENDPOINT = 'https://api.twitter.com/2/tweets/search/recent'
 
 
 def load_users() -> list[str]:
@@ -22,3 +27,36 @@ def form_queries(users: list[str]) -> list[str]:
             next_group = ['@' + user]
     queries.append(' OR '.join(next_group))
     return queries
+
+
+def make_queries(queries: list[str], target_tweets: int = 5000) -> list[str]:
+    """Get tweets from some queries."""
+    headers = {'Authorization': f'Bearer {TWITTER_TOKEN}'}
+    tweets = []
+    tweets_per_query = target_tweets // len(queries)
+    for query in queries:
+        tweets_for_query = 0
+        next_token = None
+        while (
+                    tweets_for_query < tweets_per_query and next_token
+                ) or (not tweets_for_query):
+            params = {'query': query, 'max_results': 100}
+            if next_token:
+                params['next_token'] = next_token
+            response = requests.get(
+                ENDPOINT, headers=headers, params=params
+            ).json()
+            for tweet in response['data']:
+                tweets.append(tweet['text'])
+            tweets_for_query += response['meta']['result_count']
+            next_token = response['meta'].get('next_token')
+    return tweets
+
+
+def download_tweets():
+    """Coordinates downloading a selection of tweets to a file."""
+    users = load_users()
+    queries = make_queries(users)
+    tweets = make_queries(queries)
+    with open(str(BASE_PATH / 'tweets.json'), 'w') as f:
+        json.dump(f, {'tweets': tweets})
